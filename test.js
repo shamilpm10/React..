0e21808146b9714a88f9f38edd38c61986845e11
shamilpm10 <shamilpmattil@gmail.com>
2024-09-11 19:39:21 +0530

    episode-01

--- test.js
+++ test.js
@@ -1,7 +1,8 @@
-// const heading=React.createElement("h1",{id:"head"},"hello world")
-// const root=ReactDOM.createRoot(document.getElementById("root"))
-// root.render(heading)
+//import React from "React"
+const Title=()=>(
+    <h1 ClassName="heading">
+        "hello"</h1>
 
-const heading=React.createElement("div",{id:"parent"},createElement("div",{id:"child"},[createElement("h1",{},"im an h1 tag"),createElement("h2",{},"im an h2 tag")]))
+)
 const root=ReactDOM.createRoot(document.getElementById("root"))
-root.render(heading)
+root.render(<Title/>)
